95900cba269c1e32ab9d4c68046af33bcaac20f3
Michael Roth <fjljjlol@gmail.com>
2023-03-31 18:29:53 -0400

    Deleted extraneous files

--- tt.py
+++
@@ -1,9 +0,0 @@
-import asposewordscloud as aw
-
-# Load the HTML file from disk
-doc = aw.Document("/Users/michaelroth/Downloads/test2.html")
-
-# Save the HTML file as Word DOCX document
-doc.save("html-to-word.docx")
-
-doc.
